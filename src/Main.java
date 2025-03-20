5807d82802252c0d0d3fbe2d94aab187231bae27
Laboratório FIAP <labsfiap@fiap.com.br>
2025-03-20 11:36:15 -0300

    first commit

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,7 @@
-package PACKAGE_NAME;public class Main {
+public class Main {
+    public static void main(String[] args) {
+        //Util util = new Util();
+        //util.menuPrincipal();
+        new Util().menuPrincipal();
+    }
 }
--- src/Util.java
+++ src/Util.java
@@ -1,2 +1,76 @@
-package PACKAGE_NAME;public class Util {
+import java.text.DecimalFormat;
+
+import static javax.swing.JOptionPane.*;
+import static java.lang.Integer.parseInt;
+import static java.lang.Long.parseLong;
+import static java.lang.Double.parseDouble;
+
+public class Util {
+    //banco de dados
+     private BilheteUnico[] bilhete = new BilheteUnico[2];
+     private int index = 0;
+    public void menuPrincipal() {
+        int opcao = 0;
+        String menu = "1. Adm\n2. Usuario\n3. Finalizar";
+
+        do {
+            opcao = parseInt(showInputDialog(menu));
+            switch (opcao){
+                case 1:
+                    menuAdm();
+                    break;
+            }
+        }while(opcao != 3);
+    }
+
+    private void menuAdm(){
+        int opcao;
+        String menu = "MENU ADMINISTRADOR\n";
+        menu += "1.Emitir bilhete\n";
+        menu += "2.Listar bilhetes\n";
+        menu += "3.Excluir bilhete\n";
+        menu += "4.Sair\n";
+
+        do {
+            opcao = parseInt(showInputDialog(menu));
+            switch (opcao){
+                case 1 :
+                    emitirBilhete();
+                    break;
+                case 2:
+                    listarBilhete();
+                    break;
+            }
+        }while (opcao !=4);
+
+    }
+    private void emitirBilhete() {
+        String nome, perfil;
+        long cpf;
+        if (index < bilhete.length) {
+            nome = showInputDialog("Nome do usuario :");
+            cpf = parseLong(showInputDialog("CPF : "));
+            perfil = showInputDialog("Estudante ou professor ou comum ");
+            bilhete[index] = new BilheteUnico(nome, cpf, perfil);
+            index++;
+        }
+        else {
+            showMessageDialog(null, "Procure um posto de atendimento");
+        }
+    }
+
+    private void listarBilhete() {
+        DecimalFormat df = new DecimalFormat("0.00");
+        String aux ="";
+        for (int i =0; i<index;i++) {
+            aux += "numero do bilhete : " + bilhete[i].numero + "\n";
+            aux += "saldo do bilhete :R$ " + df.format(bilhete[i].saldo) + "\n";
+            aux += "Usuario: " + bilhete[i].usuario.nome + "\n";
+            aux += "Perfil: " + bilhete[i].usuario.perfil + "\n";
+            aux += "CPF: " + bilhete[i].usuario.cpf + "\n";
+        }
+        showMessageDialog(null, aux);
+    }
+
 }
+
