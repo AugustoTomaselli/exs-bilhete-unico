aa54471f5991d918f09b28707d104fd76b60efe5
Gabriel <gabrielgaleranialmeida@gmail.com>
2025-03-27 11:13:22 -0300

    commit

--- src/Util.java
+++ src/Util.java
@@ -19,6 +19,8 @@ public class Util {
                 case 1:
                     menuAdm();
                     break;
+                case 2:
+                    menuUsuario();
             }
         }while(opcao != 3);
     }
@@ -39,6 +41,9 @@ public class Util {
                     break;
                 case 2:
                     listarBilhete();
+                    break;
+                case 3 :
+                    excluirBilhete();
                     break;
             }
         }while (opcao !=4);
@@ -72,5 +77,74 @@ public class Util {
         showMessageDialog(null, aux);
     }
 
+    private void menuUsuario() {
+        int opcao;
+        String menu = "1 Carregar bilhete\n2. Consultar saldo\n3. Passar na catraca\n4. Sair";
+        do {
+            opcao = parseInt(showInputDialog(menu));
+            if (opcao < 1 || opcao > 4) {
+                showMessageDialog(null, "Opção invalida");
+            }
+            else {
+                switch (opcao){
+                    case 1 :
+                        carregarBilhete();
+                        break;
+                    case 2:
+                        consultarSaldo();
+                        break;
+                    case 3:
+                        passarNaCatraca();
+                        break;
+                }
+            }
+        }while (opcao !=4);
+    }
+
+    private void consultarSaldo() {
+        int indice = pesquisar();
+        if (indice != -1) {
+            showMessageDialog(null , "Saldo = R$" + bilhete[indice].consultarSaldo());
+        }
+    }
+    private void carregarBilhete() {
+        int indice = pesquisar();
+        double valor;
+        if (indice != -1){
+            valor = parseDouble(showInputDialog("Valor da recarga"));
+            bilhete[indice].carregar(valor);
+        }
+    }
+    private void passarNaCatraca() {
+        int indice = pesquisar();
+        if (indice != -1) {
+            showMessageDialog(null, bilhete[indice].passarNaCatraca());
+        }
+    }
+    private int pesquisar() {
+        long cpf = parseLong(showInputDialog("CPF"));
+        for (int i=0; i< index; i++) {
+            if (bilhete[i].usuario.cpf == cpf){
+                return i;
+            }
+        }
+        showMessageDialog(null, cpf + "Não encontrado");
+        return -1;
+    }
+
+    private void excluirBilhete() {
+        int resposta;
+        int indice = pesquisar();
+        if(indice != -1) {
+            resposta = showConfirmDialog(null , "Tem certeza que deseja excluir ?");
+            if (resposta == YES_OPTION ) {
+                bilhete[indice] = bilhete[index - 1];
+                index--;
+            }
+        }
+
+    }
+
 }
+
 
